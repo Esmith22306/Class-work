6a6bf043cc3aa73a46356f3231cbb6ba3d990318
Esmith22306 <Ethankip@gmail.com>
2023-04-26 12:27:57 -0600

    commit

---
+++ abstract_example/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Book mistborn = new Book("Mistborn", "Brandon Sanderson");
+
+mistborn.Display();
+Console.WriteLine(mistborn.IsAvailable());
+
+Book blood_meridian = new Book("Blood Meridian", "Richard Poe");
+
+blood_meridian.Display();
