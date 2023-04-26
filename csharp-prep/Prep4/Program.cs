21d65ae16d2e7c0bd4f75ea7b4d077d9d3505d47
Esmith22306 <Ethankip@gmail.com>
2023-04-25 20:09:30 -0600

    commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -18,6 +18,29 @@ class Program
                 numbers.Add(userNumber);
             }
         }
-    
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Part 3: Find the max
+        // There are several ways to do this, such as sorting the list
+        
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                // if this number is greater than the max, we have found the new max!
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
