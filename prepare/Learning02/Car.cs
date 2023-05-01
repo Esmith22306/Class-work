2b5190dd90810c2aca31400423dad75fa8563f7e
Esmith22306 <Ethankip@gmail.com>
2023-05-01 12:00:27 -0600

    commit

---
+++ prepare/Learning02/Car.cs
@@ -0,0 +1,16 @@
+public class Car {
+    //instances 
+    public string brand;
+    public int miles;
+    public string color;
+
+    //constructor 
+    public Car(){
+
+    }
+
+    //Methods (functions)
+    public void move_forward(){
+        
+    }
+}
