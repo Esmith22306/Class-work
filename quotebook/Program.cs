5d746b731431cd4979b4f7003a6e897c6b486cb0
Esmith22306 <Ethankip@gmail.com>
2023-05-06 11:21:48 -0600

    commit

---
+++ quotebook/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Source source = new Source("1 Nephi 1:1");
+
+Console.WriteLine(source.Stringify());
---
+++ quotebook/quote.cs
@@ -0,0 +1,9 @@
+public class Quote
+{
+
+    private string _author = "";
+    private string _quote = "";
+    private Source _source;
+
+
+}
